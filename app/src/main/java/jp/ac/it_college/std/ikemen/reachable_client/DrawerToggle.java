377e7c7804506141eb5c4d4aed8ceeabbf36d536
Akinori Maesoko <s13012@std.it-college.ac.jp>
2015-11-17 20:56:04 +0900

    DrawerLayoutとDrawerToggleの初期化処理を実装

---
+++ app/src/main/java/jp/ac/it_college/std/ikemen/reachable_client/DrawerToggle.java
@@ -0,0 +1,27 @@
+package jp.ac.it_college.std.ikemen.reachable_client;
+
+import android.app.Activity;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class DrawerToggle extends ActionBarDrawerToggle {
+
+    public DrawerToggle(Activity activity, DrawerLayout drawerLayout, Toolbar toolbar,
+                        int openDrawerContentDescRes, int closeDrawerContentDescRes) {
+        super(activity, drawerLayout, toolbar, openDrawerContentDescRes, closeDrawerContentDescRes);
+    }
+
+    @Override
+    public void onDrawerOpened(View drawerView) {
+        super.onDrawerOpened(drawerView);
+        //ドロワーを開いた時に呼ばれる
+    }
+
+    @Override
+    public void onDrawerClosed(View drawerView) {
+        super.onDrawerClosed(drawerView);
+        //ドロワーを閉じた時に呼ばれる
+    }
+}
--- app/src/main/java/jp/ac/it_college/std/ikemen/reachable_client/MainActivity.java
+++ app/src/main/java/jp/ac/it_college/std/ikemen/reachable_client/MainActivity.java
@@ -1,10 +1,18 @@
 package jp.ac.it_college.std.ikemen.reachable_client;
 
+import android.content.res.Configuration;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    /* DrawerLayout 関連フィールド */
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mDrawerToggle;
 
     /* Toolbar 関連フィールド */
     private Toolbar mToolbar;
@@ -22,16 +30,64 @@ public class MainActivity extends AppCompatActivity {
      * 初期設定を実行
      */
     private void initSettings() {
+        //ToolBarを設定
         setUpToolbar();
+        //DrawerListenerにDrawerToggleをセット
+        getDrawerLayout().setDrawerListener(getDrawerToggle());
     }
 
     /**
      * Toolbarをアクションバーとしてセットする
      */
     private void setUpToolbar() {
-        mToolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(mToolbar);
+        setSupportActionBar(getToolbar());
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeButtonEnabled(true);
     }
+
+    public DrawerLayout getDrawerLayout() {
+        if (mDrawerLayout == null) {
+            mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        }
+
+        return mDrawerLayout;
+    }
+
+    public ActionBarDrawerToggle getDrawerToggle() {
+        if (mDrawerToggle == null) {
+            mDrawerToggle = new DrawerToggle(
+                    this, getDrawerLayout(), getToolbar(), R.string.drawer_open, R.string.drawer_close);
+        }
+
+        return mDrawerToggle;
+    }
+
+    public Toolbar getToolbar() {
+        if (mToolbar == null) {
+            mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        }
+
+        return mToolbar;
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        getDrawerToggle().syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        getDrawerToggle().onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (getDrawerToggle().onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
