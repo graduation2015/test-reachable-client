e9df9b9a985dd3a38e1ffdc071e81d41f9fe7400
Akinori Maesoko <s13012@std.it-college.ac.jp>
2015-11-17 20:21:22 +0900

    ツールバーの初期化処理を実装

--- app/src/main/java/jp/ac/it_college/std/ikemen/reachable_client/MainActivity.java
+++ app/src/main/java/jp/ac/it_college/std/ikemen/reachable_client/MainActivity.java
@@ -2,36 +2,36 @@ package jp.ac.it_college.std.ikemen.reachable_client;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    /* Toolbar 関連フィールド */
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //初期設定
+        initSettings();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    /**
+     * 初期設定を実行
+     */
+    private void initSettings() {
+        setUpToolbar();
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    /**
+     * Toolbarをアクションバーとしてセットする
+     */
+    private void setUpToolbar() {
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
     }
 }
