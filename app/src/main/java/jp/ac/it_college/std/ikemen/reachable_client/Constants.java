d2a98d7f8c54e8111bbc0c5da4b9298ccf039114
s13012 <s13012@std.it-college.ac.jp>
2015-11-20 10:44:19 +0900

    Add Constants.java

---
+++ app/src/main/java/jp/ac/it_college/std/ikemen/reachable_client/Constants.java
@@ -0,0 +1,6 @@
+package jp.ac.it_college.std.ikemen.reachable_client;
+
+public class Constants {
+    public static final String IDENTITY_POOL_ID = "";
+    public static final String BUCKET_NAME = "";
+}
