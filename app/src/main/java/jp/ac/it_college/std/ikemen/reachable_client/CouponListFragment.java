baf9a86d4bf72d27ca6a1780dd5820194de24d3b
s13012 <s13012@std.it-college.ac.jp>
2015-11-20 10:02:27 +0900

    Add CouponListFragment

---
+++ app/src/main/java/jp/ac/it_college/std/ikemen/reachable_client/CouponListFragment.java
@@ -0,0 +1,20 @@
+package jp.ac.it_college.std.ikemen.reachable_client;
+
+
+import android.app.ListFragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class CouponListFragment extends ListFragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_coupon_list, container, false);
+    }
+
+
+}
