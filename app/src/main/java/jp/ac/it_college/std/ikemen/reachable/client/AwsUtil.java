e58b1dd72bd38946a617cd63d00b878801dd2886
Akinori Maesoko <s13012@std.it-college.ac.jp>
2015-11-29 19:50:18 +0900

    不要なImport分を削除

--- app/src/main/java/jp/ac/it_college/std/ikemen/reachable/client/AwsUtil.java
+++ app/src/main/java/jp/ac/it_college/std/ikemen/reachable/client/AwsUtil.java
@@ -2,14 +2,12 @@ package jp.ac.it_college.std.ikemen.reachable.client;
 
 import android.content.Context;
 
-import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.CognitoCachingCredentialsProvider;
 import com.amazonaws.mobileconnectors.s3.transferutility.TransferUtility;
 import com.amazonaws.regions.Region;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3Client;
 
-import java.util.concurrent.ExecutionException;
 
 public class AwsUtil {
     private static CognitoCachingCredentialsProvider sCredProvider;
